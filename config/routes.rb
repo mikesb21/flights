012e4beac6c833b5306b8cc11977821f9d489843
mikesb21 <miker.sb7@gmail.com>
2020-08-13 19:48:06 -0600

    Added models, serializers and controllers for Airlines and Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, on: %i[create destroy]
+    end
+  end
+
+  get '*patch', to: 'pages#index', via: :all
 end
